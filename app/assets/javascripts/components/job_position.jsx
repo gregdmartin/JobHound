98bceeedbf8a125b6066bebf07b4aad0fbc00986
Greg Martin <gregdmartin86@gmail.com>
2024-01-24 10:52:13 -0800

    refactor JobPositionsDashboard component, data
    This moves the logic for creating the dashboard's table rows into a
    separate function, adds the check for no positions outside of the JS
    code, and massages the data for consumption by the component in the
    controller instead of embedding the logic in the view.

--- app/assets/javascripts/components/job_position.jsx
+++ app/assets/javascripts/components/job_position.jsx
@@ -1,40 +1,8 @@
-//works
-/*class TestMsg extends React.Component {
-  render() {
-    console.log("props: ", this.props);
-    return (
-      <div>working</div>
-    );
-  }
-}*/
-
-// works
-/*function TestMsg() {
-  console.log('Props:', Object.keys(this));
-  return (<div>working</div>);
-}*/
-
-// works
 const JobPositionsDashboard = props => {
-  console.log('Props: ', props);
-  let positionsContainer;
-  if(props.positions.length == 0) {
-    positionsContainer = <h3>No Positions are present with any events. Add one!</h3>
-  } else {
+  let positionsContainer = '';
+  if(props.positions.length !== 0) {
     let positionRows = props.positions.map(position => {
-      console.log("position: ", position);
-      let company = position.company.website_url === null ? position.company.name : <a href={position.company.website_url} target="_blank">{position.company.name}</a>;
-      let positionTitle = position.title + (position.domain === null ? '' : ` (${position.domain})`);
-      let positionFragment = position.job_link === null ? positionTitle : <a href={position.job_link} target="_blank">{positionTitle}</a>;
-      let interestValue = position.interest_level === null ? 'unset' : parseInt(position.interest_level) + '/5';
-
-      return (
-        <tr key={position.id} className="position_row" id={position.id}>
-          <td className="position_company">{company}</td>
-          <td className="position_title">{positionFragment}</td>
-          <td className="interest_level">{interestValue}</td>
-        </tr>
-      );
+      return positionRowElement(position);
     });
 
     positionsContainer = (
@@ -52,10 +20,26 @@ const JobPositionsDashboard = props => {
       </table>
     );
   }
+
   return (
     <div>
       <button>Add Position</button>
       {positionsContainer}
     </div>
+  );
+}
+
+positionRowElement = position => {
+  let company = position.company.website_url === null ? position.company.name : <a href={position.company.website_url} target="_blank">{position.company.name}</a>;
+  let positionTitle = position.title + (position.domain === null ? '' : ` (${position.domain})`);
+  let positionFragment = position.job_link === null ? positionTitle : <a href={position.job_link} target="_blank">{positionTitle}</a>;
+  let interestValue = position.interest_level === null ? 'unset' : parseInt(position.interest_level) + '/5';
+
+  return (
+    <tr key={position.id} className="position_row" id={position.id}>
+      <td className="position_company">{company}</td>
+      <td className="position_title">{positionFragment}</td>
+      <td className="interest_level">{interestValue}</td>
+    </tr>
   );
 }
--- app/controllers/job_positions_controller.rb
+++ app/controllers/job_positions_controller.rb
@@ -1,5 +1,9 @@
 class JobPositionsController < ApplicationController
   def index
-    @job_positions = JobPosition.all
+    @job_positions = JobPosition.all.includes(:company).map do |position|
+      # Convert model objects into hashes via `attributes`, include associated company info
+      # This makes it easier for the JS logic to work with.
+      position.attributes.merge({company: position.company.attributes})
+    end
   end
 end
